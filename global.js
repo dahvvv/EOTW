6b2078292ed56e17c019338607c01c637cb4a695
Dave Koenig <davidjosephkoenig@gmail.com>
2017-01-26 03:07:13 -0500

    Write global checkArgs func

---
+++ global.js
@@ -0,0 +1,8 @@
+function checkArgs (funcName, opts) {
+	var args = [].slice.call(arguments,2);
+	args.forEach(function(arg){
+		if (!(opts.hasOwnProperty(arg))) {
+			throw "Error: " + funcName + " requires argument '" + arg + "'";
+		}
+	});
+}
